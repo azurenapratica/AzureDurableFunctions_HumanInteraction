dd2a82a035a42798897584c905c42441d0cae4e5
Milton Camara <miltoncamaragomes@gmail.com>
2023-05-13 18:03:50 -0300

    Add project files.

---
+++ DurableFunction_Exemplos/Model/DescontoModeracaoRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DurableFunction_Exemplos.Model
+{
+    public class DescontoModeracaoRequest
+    {
+        public DescontoRequest DescontoRequest { get; set; }
+        public string ApproveRequestUrl { get; set; }
+        public string DeclineRequestUrl { get; set; }
+    }
+}
---
+++ DurableFunction_Exemplos/Model/DescontoRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DurableFunction_Exemplos.Model
+{
+    public class DescontoRequest
+    {
+        public string CodigoProduto { get; set; }
+        public decimal ValorProduto { get; set; }
+        public decimal ValorDesconto { get; set; }
+        public string SolicitadoPor { get; set; }
+    }
+}
